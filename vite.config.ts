dd11a1f1a32f4935d013d58b3582f65b93513b61
hemaoqi-Tom <hemaoqi2021@outlook.com>
2022-04-05 20:12:45 +0800

    changed build options & page title

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'Mine Sweeper',
+        },
+      },
+    }),
+  ],
 });
